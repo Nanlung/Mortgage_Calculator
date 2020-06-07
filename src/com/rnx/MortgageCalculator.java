7608faaeba750f0cf03e43ab9e8474912af39e62
Nanlung <Nanlung.temlong@gmail.com>
2020-06-07 19:06:11 +0100

    Object-Oriented commit

---
+++ src/com/rnx/MortgageCalculator.java
@@ -0,0 +1,60 @@
+package com.rnx;
+
+public class MortgageCalculator {
+    private final static byte MONTHS_YEAR = 12;
+    private final static byte PERCENT = 100;
+
+    private int principal;
+    private float annualInterest;
+    private byte years;
+
+    public MortgageCalculator(int principal, float annualInterest, byte years) {
+        this.principal = principal;
+        this.annualInterest = annualInterest;
+        this.years = years;
+    }
+
+    public double calculateBalance(short numberOfPaymentsMade) {
+
+        float numberOfPayments = getNumberOfPayments();
+        float monthlyInterest = getMonthlyInterest();
+
+        double balance = principal
+                * (Math.pow(1 + monthlyInterest,numberOfPayments) - Math.pow(1 + monthlyInterest, numberOfPaymentsMade))
+               / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+
+        return balance;
+
+    }
+
+
+    public double calculateMortgage() {
+
+
+        int numberOfPayments = getNumberOfPayments();
+        float monthlyInterest = getMonthlyInterest();
+
+        double mortgage = principal * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))/ (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+
+        return mortgage;
+
+    }
+
+    public double[] getRemainingBalance() {
+        var balances = new double[getNumberOfPayments()];
+
+        for (short month = 1; month <= years * balances.length; month++)
+            balances[month - 1] = calculateBalance(month);
+
+        return balances;
+    }
+
+
+    private int getNumberOfPayments() {
+        return years * MONTHS_YEAR;
+    }
+
+    private float getMonthlyInterest() {
+        return annualInterest / PERCENT / MONTHS_YEAR;
+    }
+}
